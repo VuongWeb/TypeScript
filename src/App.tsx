a84540e81b17c9b7e878f6ceb3a0ace19efd1eb5
VuongWeb <vuonglvph15121@fpt.edu.vn>
2022-03-17 14:15:59 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,48 @@
-import { useState } from 'react'
-// import logo from './logo.svg'
+import { useState, useEffect } from 'react'
+import {Router} from 'react-router-dom'
 import './App.css'
 import Products from './components/Products'
 import ShowInfo from './components/showInfo'
 
-interface IProduct {
-  id:number,
-  name:string
-}
+import { remove } from './api/products'
+import { IProduct } from './api/types/products'
 
-const data = [
-  {id: 1, name :"Product A"},
-  {id: 2, name :"Product B"},
-  {id: 3, name :"Product C"},
-]
-  
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [products,setProducts] = useState<IProduct>(data)
+  // const [products,setProducts] = useState<IProduct>(data)
+  const [products, setProducts] = useState<IProduct[]>([])
 
+  useEffect(() => {
+    const getProudcts = async () => {
+      const reponse = await fetch('http://localhost:8000/api/products');
+      const data = await reponse.json();
+      setProducts(data);
+    }
+    getProudcts()
+  }, [])
+
+  const removeItem = (id :number) =>{
+    remove(id)
+  }
 
   return (
     <div className="App">
       <ShowInfo name="Lê Văn Vương" age={19} />
-      {products.map(item => <Products data={item}/>)}
+      {products.map(item => {
+        return <div>{item.name} <button onClick={()=>removeItem(item._id)}>Remove</button></div>
+      })}
+      <header>
+       <ul>
+         <li></li>
+         <li></li>
+         <li></li>
+       </ul>
+     </header>
     </div>
+
+    // 
   )
 }
 
